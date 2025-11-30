1fed6a2ad6f8f565d2c38e1da71fa033e273d1ed
Tanbir <pondit.tanbir@gmail.com>
2025-11-30 18:03:57 +0600

    add: category menu hover effect

--- resources/themes/default/layouts/front-end/partials/_header.blade.php
+++ resources/themes/default/layouts/front-end/partials/_header.blade.php
@@ -238,70 +238,148 @@
 
           @php($categories = \App\Utils\CategoryManager::getCategoriesWithCountingAndPriorityWiseSorting(dataLimit: 11))
 
-          <ul class="navbar-nav mega-nav pr-lg-2 pl-lg-2 d-none d-md-block __mega-nav mr-2">
+          {{-- <ul class="navbar-nav mega-nav1 pr-md-2 pl-md-2 d-block">
             <li class="nav-item {{ !request()->is('/') ? 'dropdown' : '' }}">
-
-              <a class="nav-link dropdown-toggle category-menu-toggle-btn ps-0" href="javascript:">
+              <a class="nav-link dropdown-toggle ps-0" href="javascript:" data-toggle="dropdown">
                 <svg width="21" height="21" viewBox="0 0 21 21" fill="none"
                      xmlns="http://www.w3.org/2000/svg">
                   <path fill-rule="evenodd" clip-rule="evenodd"
                         d="M9.875 12.9195C9.875 12.422 9.6775 11.9452 9.32563 11.5939C8.97438 11.242 8.4975 11.0445 8 11.0445C6.75875 11.0445 4.86625 11.0445 3.625 11.0445C3.1275 11.0445 2.65062 11.242 2.29937 11.5939C1.9475 11.9452 1.75 12.422 1.75 12.9195V17.2945C1.75 17.792 1.9475 18.2689 2.29937 18.6202C2.65062 18.972 3.1275 19.1695 3.625 19.1695H8C8.4975 19.1695 8.97438 18.972 9.32563 18.6202C9.6775 18.2689 9.875 17.792 9.875 17.2945V12.9195ZM19.25 12.9195C19.25 12.422 19.0525 11.9452 18.7006 11.5939C18.3494 11.242 17.8725 11.0445 17.375 11.0445C16.1337 11.0445 14.2413 11.0445 13 11.0445C12.5025 11.0445 12.0256 11.242 11.6744 11.5939C11.3225 11.9452 11.125 12.422 11.125 12.9195V17.2945C11.125 17.792 11.3225 18.2689 11.6744 18.6202C12.0256 18.972 12.5025 19.1695 13 19.1695H17.375C17.8725 19.1695 18.3494 18.972 18.7006 18.6202C19.0525 18.2689 19.25 17.792 19.25 17.2945V12.9195ZM16.5131 9.66516L19.1206 7.05766C19.8525 6.32578 19.8525 5.13828 19.1206 4.4064L16.5131 1.79891C15.7813 1.06703 14.5937 1.06703 13.8619 1.79891L11.2544 4.4064C10.5225 5.13828 10.5225 6.32578 11.2544 7.05766L13.8619 9.66516C14.5937 10.397 15.7813 10.397 16.5131 9.66516ZM9.875 3.54453C9.875 3.04703 9.6775 2.57015 9.32563 2.2189C8.97438 1.86703 8.4975 1.66953 8 1.66953C6.75875 1.66953 4.86625 1.66953 3.625 1.66953C3.1275 1.66953 2.65062 1.86703 2.29937 2.2189C1.9475 2.57015 1.75 3.04703 1.75 3.54453V7.91953C1.75 8.41703 1.9475 8.89391 2.29937 9.24516C2.65062 9.59703 3.1275 9.79453 3.625 9.79453H8C8.4975 9.79453 8.97438 9.59703 9.32563 9.24516C9.6775 8.89391 9.875 8.41703 9.875 7.91953V3.54453Z"
                         fill="currentColor" />
                 </svg>
-                <span class="category-menu-toggle-btn-text">
-                  {{ translate('categories') }}
-                </span>
-              </a>
-            </li>
-          </ul>
-
-          <ul class="navbar-nav mega-nav1 pr-md-2 pl-md-2 d-block d-xl-none">
-            <li class="nav-item dropdown d-md-none">
-              <a class="nav-link dropdown-toggle ps-0" href="javascript:" data-toggle="dropdown">
-                <i class="czi-menu mt-n1 me-2 align-middle"></i>
                 <span class="me-4">
                   {{ translate('categories') }}
                 </span>
               </a>
-              <ul class="dropdown-menu __dropdown-menu-2 text-align-direction">
+              <ul class="dropdown-menu __dropdown-menu-2 text-align-direction scroll-bar">
                 @php($categoryIndex = 0)
                 @foreach ($categories as $category)
                   @php($categoryIndex++)
                   @if ($categoryIndex < 10)
-                    <li class="dropdown">
-                      <a href="{{ route('products', ['category_id' => $category['id'], 'data_from' => 'category', 'page' => 1]) }}"
-                         class="d-flex gap-10px align-items-center">
-                        <img class="aspect-1 rounded-circle" width="20"
-                             src="{{ getStorageImages(path: $category?->icon_full_url, type: 'category') }}"
-                             alt="{{ $category['name'] }}">
-                        <span>{{ $category['name'] }}</span>
-                      </a>
-                      @if ($category->childes->count() > 0)
-                        <a data-toggle='dropdown' class='__ml-50px'>
-                          <i
-                             class="czi-arrow-{{ Session::get('direction') === 'rtl' ? 'left' : 'right' }} __inline-16"></i>
+                    <li class="__inline-17">
+                      <div class="d-flex justify-content-between align-items-center">
+                        <a href="{{ route('products', ['category_id' => $category['id'], 'data_from' => 'category', 'page' => 1]) }}"
+                           class="dropdown-item d-flex gap-10px align-items-center flex-grow-1">
+                          <img class="aspect-1 rounded-circle" width="20"
+                               src="{{ getStorageImages(path: $category?->icon_full_url, type: 'category') }}"
+                               alt="{{ $category['name'] }}">
+                          <span>{{ $category['name'] }}</span>
                         </a>
-                      @endif
+                        @if ($category->childes->count() > 0)
+                          <a data-toggle='dropdown' class='header-subcategories-links __ml-50px'>
+                            <i
+                               class="czi-arrow-{{ Session::get('direction') === 'rtl' ? 'left' : 'right' }} __inline-16"></i>
+                          </a>
+                        @endif
+                      </div>
 
                       @if ($category->childes->count() > 0)
                         <ul class="dropdown-menu text-align-direction">
                           @foreach ($category['childes'] as $subCategory)
-                            <li class="dropdown">
-                              <a
-                                 href="{{ route('products', ['sub_category_id' => $subCategory['id'], 'data_from' => 'category', 'page' => 1]) }}">
-                                <span>{{ $subCategory['name'] }}</span>
-                              </a>
+                            <li class="__inline-17">
+                              <div class="d-flex justify-content-between align-items-center">
+                                <a class="dropdown-item flex-grow-1"
+                                   href="{{ route('products', ['sub_category_id' => $subCategory['id'], 'data_from' => 'category', 'page' => 1]) }}">
+                                  <span>{{ $subCategory['name'] }}</span>
+                                </a>
+                                @if ($subCategory->childes->count() > 0)
+                                  <a class="header-subcategories-links" data-toggle='dropdown'>
+                                    <i
+                                       class="czi-arrow-{{ Session::get('direction') === 'rtl' ? 'left' : 'right' }} __inline-16"></i>
+                                  </a>
+                                @endif
+                              </div>
 
                               @if ($subCategory->childes->count() > 0)
-                                <a class="header-subcategories-links" data-toggle='dropdown'>
-                                  <i
-                                     class="czi-arrow-{{ Session::get('direction') === 'rtl' ? 'left' : 'right' }} __inline-16"></i>
-                                </a>
                                 <ul class="dropdown-menu">
                                   @foreach ($subCategory['childes'] as $subSubCategory)
-                                    <li>
+                                    <li class="__inline-17">
                                       <a class="dropdown-item"
-                                         href="{{ route('products', ['sub_sub_category_id' => $subSubCategory['id'], 'data_from' => 'category', 'page' => 1]) }}">{{ $subSubCategory['name'] }}</a>
+                                         href="{{ route('products', ['sub_sub_category_id' => $subSubCategory['id'], 'data_from' => 'category', 'page' => 1]) }}">
+                                        {{ $subSubCategory['name'] }}
+                                      </a>
+                                    </li>
+                                  @endforeach
+                                </ul>
+                              @endif
+                            </li>
+                          @endforeach
+                        </ul>
+                      @endif
+                    </li>
+                  @endif
+                @endforeach
+                <li class="__inline-17">
+                  <div>
+                    <a class="dropdown-item web-text-primary" href="{{ route('categories') }}">
+                      {{ translate('view_more') }}
+                    </a>
+                  </div>
+                </li>
+              </ul>
+            </li>
+          </ul> --}}
+
+          <ul class="navbar-nav mega-nav d-block">
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle ps-0" href="javascript:" data-toggle="dropdown">
+                <svg width="21" height="21" viewBox="0 0 21 21" fill="none"
+                     xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd" clip-rule="evenodd"
+                        d="M9.875 12.9195C9.875 12.422 9.6775 11.9452 9.32563 11.5939C8.97438 11.242 8.4975 11.0445 8 11.0445C6.75875 11.0445 4.86625 11.0445 3.625 11.0445C3.1275 11.0445 2.65062 11.242 2.29937 11.5939C1.9475 11.9452 1.75 12.422 1.75 12.9195V17.2945C1.75 17.792 1.9475 18.2689 2.29937 18.6202C2.65062 18.972 3.1275 19.1695 3.625 19.1695H8C8.4975 19.1695 8.97438 18.972 9.32563 18.6202C9.6775 18.2689 9.875 17.792 9.875 17.2945V12.9195ZM19.25 12.9195C19.25 12.422 19.0525 11.9452 18.7006 11.5939C18.3494 11.242 17.8725 11.0445 17.375 11.0445C16.1337 11.0445 14.2413 11.0445 13 11.0445C12.5025 11.0445 12.0256 11.242 11.6744 11.5939C11.3225 11.9452 11.125 12.422 11.125 12.9195V17.2945C11.125 17.792 11.3225 18.2689 11.6744 18.6202C12.0256 18.972 12.5025 19.1695 13 19.1695H17.375C17.8725 19.1695 18.3494 18.972 18.7006 18.6202C19.0525 18.2689 19.25 17.792 19.25 17.2945V12.9195ZM16.5131 9.66516L19.1206 7.05766C19.8525 6.32578 19.8525 5.13828 19.1206 4.4064L16.5131 1.79891C15.7813 1.06703 14.5937 1.06703 13.8619 1.79891L11.2544 4.4064C10.5225 5.13828 10.5225 6.32578 11.2544 7.05766L13.8619 9.66516C14.5937 10.397 15.7813 10.397 16.5131 9.66516ZM9.875 3.54453C9.875 3.04703 9.6775 2.57015 9.32563 2.2189C8.97438 1.86703 8.4975 1.66953 8 1.66953C6.75875 1.66953 4.86625 1.66953 3.625 1.66953C3.1275 1.66953 2.65062 1.86703 2.29937 2.2189C1.9475 2.57015 1.75 3.04703 1.75 3.54453V7.91953C1.75 8.41703 1.9475 8.89391 2.29937 9.24516C2.65062 9.59703 3.1275 9.79453 3.625 9.79453H8C8.4975 9.79453 8.97438 9.59703 9.32563 9.24516C9.6775 8.89391 9.875 8.41703 9.875 7.91953V3.54453Z"
+                        fill="currentColor" />
+                </svg>
+                <span class="me-4">
+                  {{ translate('categories') }}
+                </span>
+              </a>
+              <ul class="dropdown-menu __dropdown-menu-2 text-align-direction scroll-bar">
+                @php($categoryIndex = 0)
+                @foreach ($categories as $category)
+                  @php($categoryIndex++)
+                  @if ($categoryIndex < 10)
+                    <li class="__inline-17">
+                      <div class="d-flex justify-content-between align-items-center">
+                        <a href="{{ route('products', ['category_id' => $category['id'], 'data_from' => 'category', 'page' => 1]) }}"
+                           class="dropdown-item d-flex gap-10px align-items-center flex-grow-1">
+                          <img class="aspect-1 rounded-circle" width="20"
+                               src="{{ getStorageImages(path: $category?->icon_full_url, type: 'category') }}"
+                               alt="{{ $category['name'] }}">
+                          <span>{{ $category['name'] }}</span>
+                        </a>
+                        @if ($category->childes->count() > 0)
+                          <a data-toggle='dropdown' class='header-subcategories-links __ml-50px'>
+                            <i
+                               class="czi-arrow-{{ Session::get('direction') === 'rtl' ? 'left' : 'right' }} __inline-16"></i>
+                          </a>
+                        @endif
+                      </div>
+
+                      @if ($category->childes->count() > 0)
+                        <ul class="dropdown-menu text-align-direction">
+                          @foreach ($category['childes'] as $subCategory)
+                            <li class="__inline-17">
+                              <div class="d-flex justify-content-between align-items-center">
+                                <a class="dropdown-item flex-grow-1"
+                                   href="{{ route('products', ['sub_category_id' => $subCategory['id'], 'data_from' => 'category', 'page' => 1]) }}">
+                                  <span>{{ $subCategory['name'] }}</span>
+                                </a>
+                                @if ($subCategory->childes->count() > 0)
+                                  <a class="header-subcategories-links" data-toggle='dropdown'>
+                                    <i
+                                       class="czi-arrow-{{ Session::get('direction') === 'rtl' ? 'left' : 'right' }} __inline-16"></i>
+                                  </a>
+                                @endif
+                              </div>
+
+                              @if ($subCategory->childes->count() > 0)
+                                <ul class="dropdown-menu">
+                                  @foreach ($subCategory['childes'] as $subSubCategory)
+                                    <li class="__inline-17">
+                                      <a class="dropdown-item"
+                                         href="{{ route('products', ['sub_sub_category_id' => $subSubCategory['id'], 'data_from' => 'category', 'page' => 1]) }}">
+                                        {{ $subSubCategory['name'] }}
+                                      </a>
                                     </li>
                                   @endforeach
                                 </ul>
@@ -331,8 +409,8 @@
 
             @if (getWebConfig(name: 'product_brand'))
               <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#"
-                   data-toggle="dropdown">{{ translate('brand') }}</a>
+                <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">{{ translate('brand') }}
+                </a>
                 <ul
                     class="text-align-direction dropdown-menu __dropdown-menu-sizing dropdown-menu-{{ Session::get('direction') === 'rtl' ? 'right' : 'left' }} scroll-bar">
                   @php($brandIndex = 0)
@@ -573,62 +651,5 @@
         </div>
       </div>
     </div>
-
-    <div class="megamenu-wrap">
-      <div class="container">
-        <div class="category-menu-wrap">
-          <ul class="category-menu">
-            @foreach ($categories as $key => $category)
-              <li>
-                <a
-                   href="{{ route('products', ['category_id' => $category['id'], 'data_from' => 'category', 'page' => 1]) }}">
-                  <span class="d-flex gap-10px justify-content-start align-items-center">
-                    <img class="aspect-1 rounded-circle" width="20"
-                         src="{{ getStorageImages(path: $category?->icon_full_url, type: 'category') }}"
-                         alt="{{ $category['name'] }}">
-                    <span class="line--limit-2">{{ $category->name }}</span>
-                  </span>
-                </a>
-                @if ($category->childes->count() > 0)
-                  <div class="mega_menu z-2">
-                    @foreach ($category->childes as $sub_category)
-                      <div class="mega_menu_inner">
-                        <h6>
-                          <a
-                             href="{{ route('products', ['sub_category_id' => $sub_category['id'], 'data_from' => 'category', 'page' => 1]) }}">{{ $sub_category->name }}</a>
-                        </h6>
-                        @if ($sub_category->childes->count() > 0)
-                          @foreach ($sub_category->childes as $sub_sub_category)
-                            <div>
-                              <a
-                                 href="{{ route('products', ['sub_sub_category_id' => $sub_sub_category['id'], 'data_from' => 'category', 'page' => 1]) }}">{{ $sub_sub_category->name }}</a>
-                            </div>
-                          @endforeach
-                        @endif
-                      </div>
-                    @endforeach
-                  </div>
-                @endif
-              </li>
-            @endforeach
-            <li class="text-center">
-              <a href="{{ route('categories') }}" class="text-primary font-weight-bold justify-content-center">
-                {{ translate('View_All') }}
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
   </div>
 </header>
-
-@push('script')
-  <script>
-    "use strict";
-
-    $(".category-menu").find(".mega_menu").parents("li")
-      .addClass("has-sub-item").find("> a")
-      .append("<i class='czi-arrow-{{ Session::get('direction') === 'rtl' ? 'left' : 'right' }}'></i>");
-  </script>
-@endpush
