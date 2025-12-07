9580588085d24ac860a7a18f866d68ac34641076
Shah Jalal <rjshahjalal132@gmail.com>
2025-12-07 13:06:05 +0600

    improved: product slider, category slider, all prducts section

--- resources/themes/greenmarket/web-views/home.blade.php
+++ resources/themes/greenmarket/web-views/home.blade.php
@@ -419,14 +419,14 @@
                     responsive: [{
                             breakpoint: 1024,
                             settings: {
-                                slidesToShow: 6,
+                                slidesToShow: 5,
                                 slidesToScroll: 1
                             }
                         },
                         {
                             breakpoint: 768,
                             settings: {
-                                slidesToShow: 4,
+                                slidesToShow: 3,
                                 slidesToScroll: 1
                             }
                         },
@@ -456,21 +456,21 @@
                         responsive: [{
                                 breakpoint: 1024,
                                 settings: {
-                                    slidesToShow: 4,
+                                    slidesToShow: 3,
                                     slidesToScroll: 1
                                 }
                             },
                             {
                                 breakpoint: 768,
                                 settings: {
-                                    slidesToShow: 3,
+                                    slidesToShow: 2,
                                     slidesToScroll: 1
                                 }
                             },
                             {
                                 breakpoint: 640,
                                 settings: {
-                                    slidesToShow: 2,
+                                    slidesToShow: 1,
                                     slidesToScroll: 1
                                 }
                             }
