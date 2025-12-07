c7a31b842334921443608614ba059558dd4da471
Shah Jalal <rjshahjalal132@gmail.com>
2025-12-07 13:06:05 +0600

    improved: product slider, category slider, all prducts section

--- resources/themes/greenmarket/web-views/partials/all-products.blade.php
+++ resources/themes/greenmarket/web-views/partials/all-products.blade.php
@@ -2,7 +2,7 @@
 <section class="py-12 bg-neutral-off-white">
     <div class="container-ds">
         <h2 class="text-3xl font-bold text-[#212529] mb-8 uppercase tracking-wider">All Products</h2>
-        <div class="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 gap-6">
+        <div class="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 gap-6">
             @php
                 $allProducts = $latestProductsList ?? collect([]);
                 $decimal_point_settings = $decimal_point_settings ?? getWebConfig(name: 'decimal_point_settings') ?? 0;
