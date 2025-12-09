6d6149d139f5465154b00827a710911d5658126e
Tanbir <pondit.tanbir@gmail.com>
2025-12-09 11:27:03 +0600

    add: temporary shortcut icon

--- routes/web/pwa.php
+++ routes/web/pwa.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('manifest.json', function () {
-    return response()->file(public_path('manifest.json'), ['Content-Type' => 'application/json']);
-});
+// Route::get('manifest.json', function () {
+//     return response()->file(public_path('manifest.json'), ['Content-Type' => 'application/json']);
+// });
