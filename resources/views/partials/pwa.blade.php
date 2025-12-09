869bde30bb790a7539485ea365230b497bf0e4a3
Tanbir <pondit.tanbir@gmail.com>
2025-12-09 09:54:09 +0600

    add: pw-files

---
+++ resources/views/partials/pwa.blade.php
@@ -0,0 +1,7 @@
+<link rel="manifest" href="/manifest.json">
+<link rel="shortcut icon" href="{{ $web_config['fav_icon']['path'] }}">
+<link rel="apple-touch-startup-image" href="/assets/icons/ios/1024.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/assets/icons/ios/180.png">
+<link rel="apple-touch-icon" sizes="152x152" href="/assets/icons/ios/152.png">
+<link rel="apple-touch-icon" sizes="120x120" href="/assets/icons/ios/120.png">
+<link rel="mask-icon" href="{{ $web_config['fav_icon']['path'] }}" color="#FFFFFF">
